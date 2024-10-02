e808ff600644a3d7f3bef738b782bb3016c91d7b
GiovanaSC <107865393+GiovanaSC@users.noreply.github.com>
2024-10-02 11:51:17 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-alert("Boas-vindas ao jogo do número secreto");
-let numeroSecreto = 10;
-let chute = prompt("Escolha um número entre 1 e 50.");
+alert("Boas-vindas ao jogo do numero secreto");
+let numeroSecreto = 34;
+let chute;
+let tentativas =20;
+
+while ( chute != numeroSecreto){
+    chute =prompt("Escolha um número entre 1 e 50");
+
+if(chute == numeroSecreto){
+    consol.log(`Isso aí!! Você descobriu o número secreto ${numerosecreto}`);
+}
+else{
+    if(numeroSecreto> chute){
+        alert(`o número secreto é maior que ${chute}`)
+    } else{
+        alert(`O número secreto é menor que ${chute}`)
+    }
+}
+}
